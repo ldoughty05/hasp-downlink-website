b8819d52479e912599a6f23888566f22e92bc7ae
Luke Doughty <luke.doughty@me.com>
2025-08-18 22:09:26 +0100

    added gunicorn config file

---
+++ flask-backend/gunicorn.conf.py
@@ -0,0 +1,16 @@
+bind = "unix:/home/pi/Git/hasp-downlink-website/hasp-downlink-website-backend.sock"
+
+workers = 2
+threads = 1
+timeout = 120
+proc_name = "haspdownlinkwebsitebackend"
+
+# Access log - records incoming HTTP requests
+accesslog = "/home/pi/Git/hasp-downlink-website/flask-backend/logs/gunicorn-haspdownlinkbackend.access.log"
+# Error log - records Gunicorn server goings-on
+errorlog = "/home/pi/Git/hasp-downlink-website/flask-backend/logs/gunicorn-haspdownlinkbackend.error.log"
+capture_output = True
+# How verbose the Gunicorn error logs should be
+loglevel = "info"
+
+pidfile = "gunicorn.pid"
